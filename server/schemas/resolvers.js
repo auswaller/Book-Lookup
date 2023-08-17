651cb5d37b4203cb02f35dd7be44675797a93331
Austin W <auswaller@gmail.com>
2023-08-17 18:04:25 -0400

    added auth and more resolver logic

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -32,10 +32,24 @@ const resolvers = {
             return { token, user };
           },
         saveBook: async (parent, { bookData }, context) => {
-
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: bookData } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
         },
         removeBook: async (parent, { bookId }, context) => {
-        
+            if (context.user) {
+                const updatedUser = await User.findOneAndDelete(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
         },
     },  
 };
