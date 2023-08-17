813b15683ce12ad74d66766b98ae184f37452c23
Austin W <auswaller@gmail.com>
2023-08-17 17:45:20 -0400

    added some of the resolver logic

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,42 @@
 const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+    },
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+          },
+          login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw AuthenticationError;
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw AuthenticationError;
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          },
+        saveBook: async (parent, { bookData }, context) => {
 
+        },
+        removeBook: async (parent, { bookId }, context) => {
+        
+        },
+    },  
 };
 module.exports = resolvers;
